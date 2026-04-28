260a39c4e1548ccf26acb9630795a766bcf42735
Praharsha2007 <anagh29p@gmail.com>
2026-04-28 13:30:31 +0530

    postre sql

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
+    db_url = os.getenv("DATABASE_URL", "sqlite:///app.db")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
