de03fbf9d71c8ab9a304d07baa7d517ceb7a0d4d
Praharsha2007 <anagh29p@gmail.com>
2026-04-28 14:06:32 +0530

    run

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from app import create_app
+import os
 
-app = create_app("app.config.DevelopmentConfig")
+app = create_app('app.config.DevelopmentConfig')
+
+@app.route('/test')
+def test():
+    db_url = os.environ.get('DATABASE_URL', 'NOT SET')
+    return {'DATABASE_URL': db_url[:30]}  # only show first 30 chars for safety
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
